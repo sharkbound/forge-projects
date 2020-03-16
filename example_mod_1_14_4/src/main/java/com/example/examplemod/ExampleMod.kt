26df925c1a3b28f66d71d3a5971ab6c52a31aa23
sharkbound <laptopblaster@gmail.com>
2020-03-16 13:34:49 -0500

    removed unneeded stuff

--- example_mod_1_14_4/src/main/java/com/example/examplemod/ExampleMod.kt
+++ example_mod_1_14_4/src/main/java/com/example/examplemod/ExampleMod.kt
@@ -28,50 +28,8 @@ class ExampleMod {
         logger.info("DIRT BLOCK >> {}", Blocks.DIAMOND_BLOCK.registryName)
     }
 
-//    fun doClientStuff(event: FMLClientSetupEvent) {
-//        // do something that can only be done on the client
-//        logger.info("Got game settings {}", event.minecraftSupplier.get().gameSettings)
-//    }
-
-//    fun enqueueIMC(event: InterModEnqueueEvent) {
-//        // some example code to dispatch IMC to another mod
-//        InterModComms.sendTo("examplemod", "helloworld") {
-//            logger.info("Hello world from the MDK")
-//            "Hello world"
-//        }
-//    }
-
-//    fun processIMC(event: InterModProcessEvent) {
-//        // some example code to receive and process InterModComms from other mods
-//        logger.info("Got IMC {}", event.imcStream.map { m: IMCMessage -> m.getMessageSupplier<Any>().get() }.toList())
-//    }
-
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
-//    @SubscribeEvent
-//    fun onServerStarting(event: FMLServerStartingEvent?) {
-//        // do something when the server starts
-//        logger.info("HELLO from server starting")
-//    }
-
-    // You can use EventBusSubscriber to automatically subscribe events on the contained class (this is subscribing to the MOD
-    // Event bus for receiving Registry Events)
-//    @SubscribeEvent
-//    fun onBlocksRegistry(blockRegistryEvent: Register<Block?>?) {
-//        // register a new block here
-//        logger.info("HELLO from Register Block")
-//    }
-
     init {
-        // Register the setup method for modloading
         modEventBus.addListener<FMLCommonSetupEvent> { setup(it) }
-//        // Register the enqueueIMC method for modloading
-//        modEventBus.addListener<InterModEnqueueEvent> { enqueueIMC(it) }
-//        // Register the processIMC method for modloading
-//        modEventBus.addListener<InterModProcessEvent> { processIMC(it) }
-//        // Register the doClientStuff method for modloading
-//        modEventBus.addListener<FMLClientSetupEvent> { doClientStuff(it) }
-
-        // Register ourselves for server and other game events we are interested in
         forgeEventBus.register(this)
     }
 }
