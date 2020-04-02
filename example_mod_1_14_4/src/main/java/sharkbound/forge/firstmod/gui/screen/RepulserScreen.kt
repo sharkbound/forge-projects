1fe167366b33e1e74adb41b1a3c917571b6024a1
sharkbound <laptopblaster@gmail.com>
2020-04-02 14:37:56 -0500

    button seems to be working, somewhat...

--- example_mod_1_14_4/src/main/java/sharkbound/forge/firstmod/gui/screen/RepulserScreen.kt
+++ example_mod_1_14_4/src/main/java/sharkbound/forge/firstmod/gui/screen/RepulserScreen.kt
@@ -2,6 +2,7 @@ package sharkbound.forge.firstmod.gui.screen
 
 import com.mojang.blaze3d.platform.GlStateManager
 import net.minecraft.client.gui.screen.inventory.ContainerScreen
+import net.minecraft.client.gui.widget.button.Button
 import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.entity.player.PlayerInventory
 import net.minecraft.inventory.container.Container
@@ -9,13 +10,17 @@ import net.minecraft.inventory.container.INamedContainerProvider
 import net.minecraft.util.ResourceLocation
 import net.minecraft.util.text.ITextComponent
 import sharkbound.forge.firstmod.MOD_ID
-import sharkbound.forge.firstmod.gui.buttons.Button1
 import sharkbound.forge.firstmod.gui.container.RepulserContainer
 import sharkbound.forge.shared.util.toText
 
 class RepulserScreen(container: RepulserContainer, val inv: PlayerInventory) : ContainerScreen<RepulserContainer>(container, inv, toText(TITLE)), INamedContainerProvider {
     val player = inv.player
-    val button = addButton(Button1(player, guiTop + 10, guiLeft + 10, 88, 52))
+    val button = addButton(Button1(player, guiTop + 10, guiLeft + 10, 50, 50))
+
+    init {
+//        xSize = 200
+//        ySize = 200
+    }
 
     override fun drawGuiContainerBackgroundLayer(partialTicks: Float, mouseX: Int, mouseY: Int) {
         GlStateManager.color4f(1f, 1f, 1f, 1f)
@@ -40,5 +45,11 @@ class RepulserScreen(container: RepulserContainer, val inv: PlayerInventory) : C
     companion object {
         const val TITLE = "&6Repulser Settings"
         val GUI = ResourceLocation(MOD_ID, "textures/gui/repulser.png")
+    }
+
+    class Button1(val player: PlayerEntity, val rx: Int, val ry: Int, width: Int, height: Int) : Button(rx, ry, width, height, "HELLO", { println("HI!") }) {
+        companion object {
+            val TEXTURE = ResourceLocation(MOD_ID, "textures/gui/button/repulser_button.png")
+        }
     }
 }
